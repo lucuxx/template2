cf75b7b0a7992df77c66577e914f8f7285de880c
lucuxx <49752634+lucuxx@users.noreply.github.com>
2020-06-24 15:11:01 +0800

    工具函数添加

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,27 @@
-export default {
-  install(Vue, options) {
-    Vue.prototype.$util = {
-      method1(val) {
-        console.log(val)
-      },
-    }
-  },
-}
+// 防抖函数
+export function debounce(fn, delay = 300) {   //默认300毫秒
+    let timer;
+    return function () {
+      let args = arguments;
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        fn.apply(this, args);   // this 指向vue
+      }, delay);
+    };
+  }
+
+// 节流函数
+export function throttle(fn, time = 300) {
+      let _arguments = arguments
+      let canRun = true 
+      return function () {
+          if (!canRun) return
+          canRun = false
+          setTimeout(() => {
+              fn.call(this, _arguments)
+              canRun = true
+          }, time);
+      }
+  }
