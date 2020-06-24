53a0afa47fa1437c8206f5caf59aa5765ddd2a8c
lucuxx <49752634+lucuxx@users.noreply.github.com>
2020-06-24 15:28:22 +0800

    Update index.js

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,5 +1,6 @@
 import loading from './Loading'
 import directiveloading from './Loading/directiveLoad'
+import autofocus from './autofocus'
 
 const install = (Vue) => {
   Object.defineProperty(Vue.prototype, '$loading', {
@@ -8,6 +9,7 @@ const install = (Vue) => {
     },
   }),
     Vue.directive('loading', directiveloading)
+    Vue.directive('autofocus', autofocus)
 }
 
 export default { install }
